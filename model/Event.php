7179b4cab7261d3046e681f16509708727be91fe
Tai Nguyen <tainguyen880s@gmail.com>
2022-11-08 16:46:40 +0700

    them 1 vai chuc nang vao model  event.php va User.php

---
+++ model/Event.php
@@ -0,0 +1,78 @@
+<?php
+require 'connect.php';
+class Event
+{
+    public function getAllEvent()
+    {
+        $conn = conn();
+        $sql = "SELECT * FROM event";
+        $result = $conn->query($sql);
+        $data = array();
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while ($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+        }
+        var_dump($data);
+    }
+
+
+    // event
+    public function inserNewEvent()
+    {
+        $id_user = $_POST['id_user'];
+        $name_event = $_POST['name_event'];
+        $img_event = $_FILES['img_event']['name'];
+
+
+        $target_dir = "../uploads/";
+        $target_file = $target_dir . basename($_FILES['img_event']['name']);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+
+        // validate file img 
+        if (isset($_POST["submit"])) {
+            $check = getimagesize($_FILES['img_event']['name']);
+            if ($check !== false) {
+                echo "File is an image - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            } else {
+                echo "File is not an image.";
+                $uploadOk = 0;
+            }
+        }
+
+        // Check file size
+        if ($_FILES['img_event']['name'] > 500000) {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+
+        // Allow certain file formats
+        if (
+            $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif"
+        ) {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }
+
+
+        move_uploaded_file($_FILES["img_event"]["tmp_name"], $target_file);
+
+        $conn = conn();
+        $sql = "INSERT INTO event (
+            id_user,
+            name_event,
+            img_event
+        ) VALUES (
+            '$id_user',
+            '$name_event',
+            '$img_event'
+        )";
+        $result = $conn->query($sql);
+        return $result;
+    }
+}
