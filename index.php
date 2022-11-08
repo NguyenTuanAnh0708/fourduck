e64242d4ef1b59579840a10f57ab73f17a250bb9
Tai Nguyen <tainguyen880s@gmail.com>
2022-11-09 00:03:33 +0700

    đổi thành file PHP và thêm href=index.php vào home trong nav

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
 <?php
 $admin = true;
 if ($admin) {
+    error_reporting(0);
+    $url = null;
     $url = $_GET['url'];
-    include './view/admin/headerAdmin.html';
+    include './view/admin/headerAdmin.php';
     switch ($url) {
         case 'clienMange':
-            include './view/admin/khachhang.html';
+            include './view/admin/khachhang.php';
             break;
         case 'event':
-            include './view/admin/event.html';
+            include './view/admin/event.php';
             break;
         case 'hoadon':
-            include './view/admin/hoadon.html';
+            include './view/admin/hoadon.php';
             break;
         case 'category':
-            include './view/admin/loaisp.html';
+            include './view/admin/loaisp.php';
             break;
         default:
-            include './view/admin/admin.html';
+            include './view/admin/admin.php';
     }
-    include './view/admin/footerAdmin.html';
+    include './view/admin/footerAdmin.php';
 }
