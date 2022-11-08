a287fcbd3006ae2f70d2d49052f82d01e97bc26e
teo <tuananhnguyenbsbd@gmail.com>
2022-11-08 23:38:59 +0700

    full frnt end

--- index.php
+++ index.php
@@ -1,2 +1,23 @@
 <?php
 $admin = true;
+if ($admin) {
+    $url = $_GET['url'];
+    include './view/admin/headerAdmin.html';
+    switch ($url) {
+        case 'clienMange':
+            include './view/admin/khachhang.html';
+            break;
+        case 'event':
+            include './view/admin/event.html';
+            break;
+        case 'hoadon':
+            include './view/admin/hoadon.html';
+            break;
+        case 'category':
+            include './view/admin/loaisp.html';
+            break;
+        default:
+            include './view/admin/admin.html';
+    }
+    include './view/admin/footerAdmin.html';
+}
