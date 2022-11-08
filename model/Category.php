37e174a1d26f02a3195afbd3a50ce509603e6752
Tai Nguyen <tainguyen880s@gmail.com>
2022-11-08 22:50:31 +0700

    them function inserCartegory

--- model/Category.php
+++ model/Category.php
@@ -23,4 +23,29 @@ class Category
         $result = $conn->query($sql);
         return $result;
     }
+
+
+    public function inserCartegory()
+    {   
+        $id_user = $_POST['id_user'];
+        $name_event = $_POST['name_event'];
+        $img_event = $_FILES['img_event']['name'];
+        $img_event_tmp = $_FILES['img_event']['tmp'];
+
+        move_uploaded_file($img_event_tmp, "../uploads/" . $img_event);
+
+        $conn = conn();
+        $sql = "INSERT INTO event (
+            id_user,
+            name_event,
+            img_event
+        ) VALUES (
+            '$id_user',
+            '$name_event',
+            '$img_event'
+        )";
+        $result = $conn -> query($sql);
+        return $result;
+    }
+
 }
